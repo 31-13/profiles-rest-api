640cd8cd4dd0763e3dd67313592a5f744080946d
Ricardo <ricardin.menotti@gmail.com>
2021-03-20 11:27:36 -0700

    Added viewset to profiles api

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -1,6 +1,7 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
+from rest_framework import viewsets
 
 from profiles_api import serializers
 
@@ -43,3 +44,44 @@ class HelloApiView(APIView):
     def delete(self, request, pk = None):
         """Delete an object"""
         return Response({'method': 'DELETE'})
+
+class HelloViewSet(viewsets.ViewSet):
+    """Test api view set"""
+    serializer_class = serializers.HelloSerializer
+    def list(self, request):
+        """Return a Hello message"""
+
+        a_viewset = [
+         "uses actions(list, create, retrieve, udpate, partial_update)",
+         "automatically maps to URLs using Routers",
+         "Provides more functionality with less code"
+        ]
+        return Response({"message" : "hello!", "a_viewset": a_viewset})
+
+    def create(self, request):
+        """Create a new hellow message"""
+        serializer = self.serializer_class(data = request.data)
+        if serializer.is_valid():
+            name = serializer.validated_data.get('name')
+            message = f"hello {name}!"
+            return Response({'message' : message})
+        return Response(serializer.errors,
+                    status = status.HTTP_400_BAD_REQUEST
+                    )
+
+    def retrieve(self, request, pk = None):
+        """Handle object by id"""
+
+        return Response({"http_method" : "GET"})
+
+    def update(self, request, pk = None):
+        """Hanlde updating object"""
+        return Response({"http_method" : "PUT"})
+
+    def partial_update(self, request, pk = None):
+        """Hanlde update part of an object"""
+        return Response({'http_method' : 'PATCH'})
+
+    def destroy(self, request, pk = None):
+        """Remove an object"""
+        return Response({'http_method' : 'DELETE'})
